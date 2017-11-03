2e1a80d7e44597353af6263f13ba750ea1b64289
Kyle1993 <344927747@qq.com>
2017-11-03 10:37:18 +0800

    update

--- gym_starcraft/envs/simple_battle_env.py
+++ gym_starcraft/envs/simple_battle_env.py
@@ -30,13 +30,15 @@ class Unit_State(object):
         self.pixel_size_x = unit.pixel_size_x
         self.pixel_size_y = unit.pixel_size_y
         self.delta_health = 0
+        self.delta_shield = 0
 
     def update(self,unit):
         self.delta_health = self.health-unit.health
         self.health = unit.health
+        self.delta_shield = self.shield-unit.shield
+        self.shield = unit.shield
         self.x = unit.x
         self.y = unit.y
-        self.shield = unit.shield
         self.attackCD = unit.groundCD
         self.velocityX = unit.velocityX        # speed
         self.velocityY = unit.velocityY
@@ -58,24 +60,28 @@ class Unit_State(object):
 
 
 class SimpleBattleEnv(sc.StarCraftEnv):
-    def __init__(self, server_ip, server_port,hyperparameters, speed=0, self_play=False):
-        super(SimpleBattleEnv, self).__init__(server_ip, server_port, speed,
-                                              hyperparameters['FRAME_SKIP'], self_play,
-                                              hyperparameters['MAX_STEP'])
-
+    def __init__(self, server_ip, server_port,MYSELF_NUM,ENEMY_NUM,ACTION_NUM,DISTANCE_FACTOR=10,POSITION_RANGE=400,SCREEN_BOX=((40, 105), (150, 170)),
+                 DIE_REWARD=2e-3,HEALTH_REWARD_WEIGHT=4,WIN_REWARD_WEIGHT=1,MY_HEALTH_WEIGHT=1,ENEMY_HEALTH_WEIGHT=4,FRAME_SKIP=2,MAX_STEP=700,speed=0, self_play=False):
+        super(SimpleBattleEnv, self).__init__(server_ip, server_port, speed,FRAME_SKIP, self_play,MAX_STEP)
         self.mapname = None
+        # basic config
+        self.MYSELF_NUM = MYSELF_NUM
+        self.ENEMY_NUM = ENEMY_NUM
+        self.ACTION_NUM = ACTION_NUM
 
-        self.MYSELF_NUM = hyperparameters['MYSELF_NUM']
-        self.ENEMY_NUM = hyperparameters['ENEMY_NUM']
-        self.DISTANCE_FACTOR = hyperparameters['DISTANCE_FACTOR']
-        self.ACTION_NUM = hyperparameters['NB_ACTION']
-        self.SCREEN_BOX = hyperparameters['SCREEN_BOX']  # (left_top,rigth_down)
-        self.POSITION_RANGE = hyperparameters['POSITION_RANGE']
-        self.DIE_REWARD = hyperparameters['DIE_REWARD']  # 500 means max_step
-        self.HEALTH_REWARD_WEIGHT = hyperparameters['HEALTH_REWARD_WEIGHT']
-        self.WIN_REWARD_WEIGHT = hyperparameters['WIN_REWARD_WEIGHT']
+        # env config
+        self.DISTANCE_FACTOR = DISTANCE_FACTOR
+        self.SCREEN_BOX = SCREEN_BOX     # (left_top,rigth_down)
+        self.POSITION_RANGE = POSITION_RANGE
 
-        self.myself_alive_list = None    # like [1,1,0,0,1]
+        # reward compute
+        self.DIE_REWARD = DIE_REWARD
+        self.HEALTH_REWARD_WEIGHT = HEALTH_REWARD_WEIGHT
+        self.WIN_REWARD_WEIGHT = WIN_REWARD_WEIGHT
+        self.MY_HEALTH_WEIGHT = MY_HEALTH_WEIGHT
+        self.ENEMY_HEALTH_WEIGHT = ENEMY_HEALTH_WEIGHT
+
+        self.myself_alive_list = None    # like [1,1,0,0,1],init by first state
         self.enemy_alive_list = None
 
         self.current_state = None  # current_state['myself']: units state of myself; current_state['enemy']:units of enemy
@@ -90,6 +96,8 @@ class SimpleBattleEnv(sc.StarCraftEnv):
 
         self.init_myself_total_hp = None  # in reward compute
         self.init_enemy_total_hp = None
+        self.init_myself_total_shield = None
+        self.init_enemy_total_shield = None
 
         print('------------- Enveriment config ---------------')
         print('ip:',server_ip)
@@ -104,9 +112,9 @@ class SimpleBattleEnv(sc.StarCraftEnv):
         return spaces.Box(np.array(action_low), np.array(action_high))
 
 
-    # needs to be modified
+    # return the current_state, have no space, you can extract the state you want
     def _observation_space(self):
-        print('return the current_state, have no space, you can extract the state you want')
+        # print('return the current_state, have no space, you can extract the state you want')
         return spaces.Box(np.array([1.]),np.array([1.]))
 
     def _make_commands(self, actions):
@@ -121,19 +129,23 @@ class SimpleBattleEnv(sc.StarCraftEnv):
             unit = self.current_state['myself'][uid]
 
             if not unit.die:   # if not die
-                if actions[uid][0]>0:
+                if actions[uid][0]>=0:
                     # attack
                     enemy_id =self.get_closest_enemy2(unit,actions[uid])  # get the colse enemy of target position
-                    if enemy_id is None:
-                        x_ = actions[uid][1] * self.DISTANCE_FACTOR
-                        y_ = actions[uid][2] * self.DISTANCE_FACTOR
-                        x_target,y_target = self.get_position2(unit.x,unit.y,x_,y_)
-                        x_target,y_target = max(x_target,self.SCREEN_BOX[0][0]),max(y_target,self.SCREEN_BOX[0][1])
-                        x_target,y_target = min(x_target,self.SCREEN_BOX[1][0]),min(y_target,self.SCREEN_BOX[1][1])
-                        cmd = [tcc.command_unit_protected, unit.id, tcc.unitcommandtypes.Move, -1, int(x_target), int(y_target)]
-                    else:
+                    # if enemy_id is None:
+                    #     x_ = actions[uid][1] * self.DISTANCE_FACTOR
+                    #     y_ = actions[uid][2] * self.DISTANCE_FACTOR
+                    #     x_target,y_target = self.get_position2(unit.x,unit.y,x_,y_)
+                    #     x_target,y_target = max(x_target,self.SCREEN_BOX[0][0]),max(y_target,self.SCREEN_BOX[0][1])
+                    #     x_target,y_target = min(x_target,self.SCREEN_BOX[1][0]),min(y_target,self.SCREEN_BOX[1][1])
+                    #     cmd = [tcc.command_unit_protected, unit.id, tcc.unitcommandtypes.Move, -1, int(x_target), int(y_target)]
+                    # else:
+                    #     cmd = [tcc.command_unit_protected, unit.id, tcc.unitcommandtypes.Attack_Unit, enemy_id]
+
+                    if enemy_id is not None:
                         cmd = [tcc.command_unit_protected, unit.id, tcc.unitcommandtypes.Attack_Unit, enemy_id]
-                        # cmds.append(cmd)
+                    else:
+                        continue
                 else:
                     # move
                     # degree = actions[uid][1] * 180
@@ -170,8 +182,10 @@ class SimpleBattleEnv(sc.StarCraftEnv):
     # provide two options,total_reward or separate_reward, you can even calculate by yourself
     def _compute_reward(self):
 
-        reward = self.compute_reward_separately2()
         # reward = self.compute_reward_total()
+        reward = self.compute_reward_separately()
+        # reward = self.compute_reward_separately2()
+
 
         return reward
 
@@ -197,46 +211,36 @@ class SimpleBattleEnv(sc.StarCraftEnv):
 
         myself_detla_health = 0
         enemy_detla_health = 0
+        myself_detla_shield = 0
+        enemy_detla_shield = 0
+
         for unit in self.current_state['enemy']:
-            enemy_detla_health += unit.delta_health
-        enemy_detla_health = float(enemy_detla_health) / float(self.init_enemy_total_hp)
+            enemy_detla_health += max(0,unit.delta_health)
+            enemy_detla_shield += max(0,unit.delta_shield)
+        enemy_detla_health = float(enemy_detla_health+enemy_detla_shield)/float(self.init_enemy_total_hp+self.init_enemy_total_shield)
 
         for unit in self.current_state['myself']:
             if unit.die:
                 rewards.append(self.DIE_REWARD)
             else:
-                myself_detla_health += unit.delta_health
-                unit_reward = self.ENEMY_NUM*enemy_detla_health-unit.delta_health/unit.max_health
-                rewards.append(self.HEALTH_REWARD_WEIGHT*unit_reward)
-        myself_detla_health = float(myself_detla_health) / float(self.init_myself_total_hp)
+                myself_detla_health += max(0,unit.delta_health)
+                myself_detla_shield += max(0,unit.delta_shield)
+                health_reward = self.ENEMY_HEALTH_WEIGHT*enemy_detla_health - self.MY_HEALTH_WEIGHT*(max(0,unit.delta_health+unit.delta_shield))/(unit.max_health+unit.max_shield)
+                win_reward = float(self.win) * float(sum(self.myself_alive_list))
+                rewards.append(self.HEALTH_REWARD_WEIGHT * health_reward + self.WIN_REWARD_WEIGHT * win_reward)
 
-        health_reward = self.HEALTH_REWARD_WEIGHT * (enemy_detla_health - myself_detla_health)
-        win_reward = float(self.win) * float(sum(self.myself_alive_list))
-        global_reward = health_reward + win_reward
-        rewards.insert(0,global_reward)
+        # print('myself_detla_health',myself_detla_health)
+        # print('myself_detla_shield',myself_detla_shield)
+        # print('enemy_detla_health',enemy_detla_health)
+        # print('enemy_detla_shield',enemy_detla_shield)
+        # print(rewards)
+        rewards.insert(0,sum(rewards)/self.MYSELF_NUM)
 
         return rewards
 
     def compute_reward_separately2(self):
 
         rewards = []
-
-        myself_detla_health = 0
-        enemy_detla_health = 0
-        for unit in self.current_state['enemy']:
-            enemy_detla_health += unit.delta_health
-        enemy_detla_health = float(enemy_detla_health) / float(self.init_enemy_total_hp)
-
-        for unit in self.current_state['myself']:
-            if unit.die:
-                rewards.append(self.DIE_REWARD)
-            else:
-                myself_detla_health += unit.delta_health
-                health_reward = unit.attacking*max(1,float(self.ENEMY_NUM)/float(self.MYSELF_NUM))*enemy_detla_health # -unit.delta_health/unit.max_health
-                win_reward = float(self.win) * float(sum(self.myself_alive_list)) * float(self.ENEMY_NUM)/float(self.MYSELF_NUM)
-                rewards.append(self.HEALTH_REWARD_WEIGHT*health_reward+self.WIN_REWARD_WEIGHT*win_reward)
-
-        rewards.insert(0,sum(rewards)/self.MYSELF_NUM)
 
         return rewards
 
@@ -260,9 +264,16 @@ class SimpleBattleEnv(sc.StarCraftEnv):
         self.myself_unit_dict = None
         self.enemy_unit_dict = None
 
-
-        self.init_myself_total_hp = None
-        self.init_enemy_total_hp = None
+        self.init_myself_total_hp = 0
+        self.init_enemy_total_hp = 0
+        self.init_myself_total_shield = 0
+        self.init_enemy_total_shield = 0
+        for unit in self.state.units[0]:
+            self.init_myself_total_hp += unit.health
+            self.init_myself_total_shield += unit.shield
+        for unit in self.state.units[1]:
+            self.init_enemy_total_hp += unit.health
+            self.init_enemy_total_shield += unit.shield
 
         self.advanced_termination = True
 
@@ -307,6 +318,9 @@ class SimpleBattleEnv(sc.StarCraftEnv):
 
         self.myself_current_hp = 0
         self.enemy_current_hp = 0
+        self.myself_current_shield = 0
+        self.enemy_current_shield = 0
+
         self.mapname = self.state.map_name
 
         # if current_state is init_state
@@ -319,23 +333,20 @@ class SimpleBattleEnv(sc.StarCraftEnv):
             self.myself_unit_dict = {}
             self.current_state['myself'] = [] 
             self.myself_alive_list = [1 for _ in range(self.MYSELF_NUM)]
-            # self.init_myself_total_hp = 0
             for unit_index,unit in enumerate(self.state.units[0]):
                 self.myself_unit_dict[unit.id]= unit_index
                 self.current_state['myself'].append(Unit_State(unit,unit.id))
                 self.myself_current_hp += unit.health
-            self.init_myself_total_hp = self.myself_current_hp
-                # self.init_myself_total_hp += unit.max_health
-        # current_state is not init_state
+                self.myself_current_shield += unit.shield
         else:
             self.myself_alive_list = [0 for _ in range(self.MYSELF_NUM)]
-            # self.current_state['myself'] = [None for _ in range(self.MYSELF_NUM)]
             for unit in self.state.units[0]:
                 uid = self.myself_unit_dict[unit.id]
                 self.current_state['myself'][uid].update(unit)
                 self.myself_alive_list[uid] = 1
                 self.myself_current_hp += unit.health
-            for index in range(self.MYSELF_NUM):        # if the unit die, set None
+                self.myself_current_shield += unit.shield
+            for index in range(self.MYSELF_NUM):        # if the unit die, set die
                 if self.myself_alive_list[index]==0:
                     self.current_state['myself'][index].set_die()
 
@@ -346,14 +357,11 @@ class SimpleBattleEnv(sc.StarCraftEnv):
             self.enemy_unit_dict = {}
             self.current_state['enemy'] = []
             self.enemy_alive_list = [1 for _ in range(self.ENEMY_NUM)]
-            # self.init_enemy_total_hp = 0
             for unit_index,unit in enumerate(self.state.units[1]):
                 self.enemy_unit_dict[unit.id]= unit_index
                 self.current_state['enemy'].append(Unit_State(unit,unit.id))
                 self.enemy_current_hp += unit.health
-            self.init_enemy_total_hp = self.enemy_current_hp
-                # self.init_enemy_total_hp += unit.max_health
-        # current_state is not init_state
+                self.enemy_current_shield += unit.shield
         else:
             self.enemy_alive_list = [0 for _ in range(self.ENEMY_NUM)]
             # self.current_state['enemy'] = [None for _ in range(self.ENEMY_NUM)]
@@ -362,16 +370,10 @@ class SimpleBattleEnv(sc.StarCraftEnv):
                 self.current_state['enemy'][uid].update(unit)
                 self.enemy_alive_list[uid] = 1
                 self.enemy_current_hp += unit.health
+                self.enemy_current_shield += unit.shield
             for index in range(self.ENEMY_NUM):
                 if self.enemy_alive_list[index]==0:
                     self.current_state['enemy'][index].set_die()
-
-
-        # # init the init_total_hp
-        # if self.init_myself_total_hp is None:
-        #     self.init_myself_total_hp = self.myself_current_hp
-        # if self.init_enemy_total_hp is None:
-        #     self.init_enemy_total_hp = self.enemy_current_hp
 
         assert len(self.current_state['myself']) == self.MYSELF_NUM
         assert len(self.myself_alive_list) == self.MYSELF_NUM
@@ -379,6 +381,30 @@ class SimpleBattleEnv(sc.StarCraftEnv):
         assert len(self.current_state['enemy']) == self.ENEMY_NUM
         assert len(self.enemy_alive_list) == self.ENEMY_NUM
         assert int(sum(self.enemy_alive_list)) == len(self.state.units[1])
+
+        # print('------------------------')
+        # print('my hp:       ',self.init_myself_total_hp,self.myself_current_hp)
+        # print('my shield:   ',self.init_myself_total_shield,self.myself_current_shield)
+        # print('enemy hp:    ',self.init_enemy_total_hp,self.enemy_current_hp)
+        # print('enemy shield:',self.init_enemy_total_shield,self.enemy_current_shield)
+        #
+        # my_detla_health = []
+        # my_detla_shidel = []
+        # my_maxshield = []
+        # enemy_detla_health = []
+        # enemy_detla_shidel = []
+        # for unit in self.current_state['myself']:
+        #     my_detla_health.append(unit.delta_health)
+        #     my_detla_shidel.append(unit.delta_shield)
+        #     my_maxshield.append(unit.max_shield)
+        # for unit in self.current_state['enemy']:
+        #     enemy_detla_health.append(unit.delta_health)
+        #     enemy_detla_shidel.append(unit.delta_shield)
+        # print('my_detla_health:', my_detla_health)
+        # print('my_detla_shidel:', my_detla_shidel)
+        # print('enemy_detla_health:', enemy_detla_health)
+        # print('enemy_detla_shidel:', enemy_detla_shidel)
+        # print('my_maxshield',my_maxshield)
 
     # return done
     def _check_done(self):
